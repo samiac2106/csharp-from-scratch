28071cb6dc85e77727baf9efa662841e77a1c98d
Sandra Milena Acevedo <samiac2106@gmail.com>
2026-04-05 11:30:05 -0500

    nuevos cambios

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace Helloworld
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Name = "Jhon";       
+            Console.WriteLine($"Hello World!{Name}");
+            Console.WriteLine();
+        }
+    }
+}
